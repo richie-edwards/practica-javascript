8df92079485eb16a3db7322da48d99e098ea18db
Richie Edwards <richie00edwards@gmail.com>
2023-07-22 10:12:13 -0400

    repaso de capitulo 1

---
+++ capitulo_1/ejercicios.js
@@ -0,0 +1,24 @@
+/*
+Resumen:
+- bits
+- typeof
+- boolean
+- concatenate
+- \n
+- logical
+- ternary
+- empty values (null, undefined)
+- automatic type conversion
+- == vs ===
+*/
+
+/* 
+Dado un numero, representando la edad de alguien
+escribe codigo que determina si esa persona es mayor
+de edad. Si no es mayor de edad, calcula cuantos años le quedan para 
+llegar a ser mayor de edad.
+
+mayor de edad = mayor o igual 18
+menor de edad = menor de 18 
+ß
+*/
