c7930eb786b4310a8c10f059b477d28c3267eb3f
Jimmy <zaybernez@gmail.com>
2023-07-22 11:10:02 -0400

    resumen con respuestas

--- capitulo_1/ejercicios.js
+++ capitulo_1/ejercicios.js
@@ -1,15 +1,60 @@
 /*
 Resumen:
-- bits
-- typeof
-- boolean
-- concatenate
-- \n
-- logical
+- bits: 
+1           0       1
+2^2=4       2^1=2    2^0=1
+101 = 5
+   
+- typeof: saber tipo de data que se esta usando
+
+- boolean: 
+falso o verdadero
+if(<boolean>){
+    entro aqui se boolean es verdadero
+}
+0 es falso
+if(0){
+    // no va entrar porque 0 es falso
+}
+
+if([]){
+    // no va entrar porque [] array esta vacio
+}
+
+- concatenate: unir resultado de variables, no sumar, pero unificar
+- \n:^salto de lineo 
+- logical: && (and), ||  (or) -> semejante a la tabla de verdad
+F || F = F
+V || F = V
+F || V = V
+V || V = V
+
+F && F = F
+V && F = F
+F && V = F
+V && V = V
+
 - ternary
+let hembra = falso;
+let vestuario = hembra ? primerValor : segundoValor
+vestuario es primerValor porque la pregunata resulto en ser verdader/true
+
+let hembra = true;
+let vestuario = hembra ? falda : pantalon
+vestuario es falsa
+
 - empty values (null, undefined)
 - automatic type conversion
+
+
 - == vs ===
+==: comparasion no stricto
+===: comparasion , per mas especifico/stricto
+
+
+"5" == 5 (es verdadero porque llevo numero a string)
+"5" === 5 (es falso porque es stricto y no trata de convertir )
+
 */
 
 /* 
@@ -20,5 +65,9 @@ llegar a ser mayor de edad.
 
 mayor de edad = mayor o igual 18
 menor de edad = menor de 18 
-ß
+
+let edad = 20
+.... tu codigo aqui ....
+
+
 */
