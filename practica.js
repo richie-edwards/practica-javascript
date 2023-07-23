303fabb9711fd46a1d750cd92459b14e71474ded
Jimmy <zaybernez@gmail.com>
2023-07-23 00:34:58 -0400

    Practica 1 Actualizada

--- practica.js
+++
@@ -1,8 +0,0 @@
-
-let elNumero = Number(prompt("Elige un numero"));
-if (!Number.isNaN(elNumero)) {
-  console.log("Tu número es la raiz cuadrada de " +
-              elNumero * elNumero);
-} else {
-  console.log("Ey. Por qué no me diste un número?");
-}
